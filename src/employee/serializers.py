513509f2c62099133fc66db73bd76f72b9bd03b1
Mohammed Ali <mohaali482@gmail.com>
2022-07-24 19:14:50 +0300

    Add serializer for employee model

---
+++ src/employee/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from .models import Employee
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    # id = serializers.HyperlinkedIdentityField("employee-detail")
+
+    class Meta:
+        model = Employee
+        fields = "__all__"
