d42bd43d3fd4d79ccc9df5c4695909fb0f0ce4ff
Mohammed Ali <mohaali482@gmail.com>
2023-01-29 14:14:10 +0300

    Sorted imports of user/serializers.py

--- src/users/serializers.py
+++ src/users/serializers.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import Permission, Group
+from django.contrib.auth.models import Group, Permission
 from rest_framework import serializers
 
 User = get_user_model()
